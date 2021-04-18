52985161c56638d9c0add443cbc19cec5dac10b4
flcsn <73377890+flcsn@users.noreply.github.com>
2021-04-18 19:11:36 +0800

    completed redo of TodoList Java practice exercise

---
+++ TodoList2/src/main/java/UserInterface.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Frankie
+ */
+import java.util.Scanner;
+
+public class UserInterface {
+    private TodoList todoList;
+    private Scanner scanner;
+    
+    public UserInterface(TodoList todoList, Scanner scanner) {
+        this.todoList = todoList;
+        this.scanner = scanner;
+    }
+    
+    public void start() {
+        while(true) {
+            System.out.print("Command: ");
+            String command = scanner.nextLine();
+            
+            if (command.equals("stop")) {
+                break;
+            }
+            
+            if (command.equals("add")) {
+                System.out.print("To add: ");
+                String task = scanner.nextLine();
+                todoList.add(task);
+                
+            } else if (command.equals("list")) {
+                todoList.print();
+                
+            } else if (command.equals("remove")) {
+                System.out.print("Which one is removed? ");
+                int index = Integer.valueOf(scanner.nextLine());
+                todoList.remove(index);
+            }
+        }
+    }
+}
