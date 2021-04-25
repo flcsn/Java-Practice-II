2198d9a6d6b8860376fcfe799f09cc8c005aa911
flcsn <73377890+flcsn@users.noreply.github.com>
2021-04-25 18:43:12 +0800

    completed RegularExpressions Java practice exercise
    first look at using Regular Expressions

---
+++ RegularExpressions/src/main/java/Checker.java
@@ -0,0 +1,25 @@
+
+
+public class Checker {
+    
+    public boolean isDayOfWeek(String string) {
+        String regex = "mon|tue|wed|thu|fri|sat|sun";
+        
+        if (string.matches(regex)) return true;
+        return false;
+   }
+    
+    public boolean allVowels(String string) {
+        String regex = "(a|e|i|o|u)*";
+        
+        if (string.matches(regex)) return true;
+        return false;
+    }
+    
+    public boolean timeOfDay(String string) {
+        String regex = "([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
+        
+        if (string.matches(regex)) return true;
+        return false;
+    }
+}
